5f9eced4016b56bfb18d5d2a93b74344a8bb248a
lorenzo <lorenzo.romeo.mail@gmail.com>
2023-02-06 21:51:42 +0100

    commit

--- echo_servo.ino
+++ echo_servo.ino
@@ -1,24 +1,35 @@
 #include <Servo.h>
 
 //#define ENABLE_RADAR
-//#define ENABEL_MOTORS
-#define SERVO_PIN 5
-#define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
+#define ENABLE_MOTORS
 
-#define rf 12
-#define rb 11
-#define lf 9
-#define lb 10
+// servo
+#define SERVO_PIN 5
+
+// sensore ultrasonico
+#define echoPin 2 
+#define trigPin 3 
+
+// sensori hall effect
+#define hall_pin_r A0
+#define hall_pin_l A1
+
+// controlli per il ponte h
+#define rf 10 // right forward
+#define rb 9  // right backwards
+#define lf 11 // left forwards
+#define lb 12 // left backwards
 
 #define WIDTH 21
 #define LENGTH 21
 
-// defines variables
-long duration; // variable for the duration of sound wave travel
-int distance; // variable for the distance measurement
-Servo myservo;  // create servo object to control a servo
-int pos = 0; 
+// variabil per il sensore ultrasonico
+long duration;  // variable for the duration of sound wave 
+int distance;   // variable for the distance measurement
+
+// servo
+Servo myservo;  
+int pos = 0;  //posizione corrente del servo
 
 struct location {
   int x;
@@ -28,6 +39,7 @@ struct location {
 
 struct location *loc;
 
+// stampa la mappa. 
 void p_map(){
   char buf[5];
   for(short i=0; i<LENGTH; i++){
@@ -43,6 +55,7 @@ void p_map(){
   }
 }
 
+//inizializza la matrice della mappa. Imposta i limiti della matrice a -1 (ostacolo invalicabile)
 void init_loc(){
   loc = malloc(sizeof(struct location));
   loc -> x = LENGTH/2;
@@ -59,30 +72,7 @@ void init_loc(){
   }  
 }
 
-void setup() {
-  #ifdef ENABLE_RADAR
-    myservo.attach(SERVO_PIN);
-    pinMode(trigPin, OUTPUT); 
-    pinMode(echoPin, INPUT); 
-  #endif
-  #ifdef ENABLE_MOTORS
-    pinMode(rf, OUTPUT); 
-    pinMode(rb, OUTPUT);
-    pinMode(lf, OUTPUT);
-    pinMode(lb, OUTPUT);
-  #endif
-
-  Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
-
-  init_loc();
-  p_map();
-    
-  Serial.println("---"); // print some text in Serial Monitor
-  myservo.write(pos);
-  delay(1000);
-}
-
-
+// print array (al monitor seriale)
 void printa(size_t size, int *arr){
   for(int i=0; i<size; i++){
     Serial.print(arr[i]);
@@ -91,25 +81,21 @@ void printa(size_t size, int *arr){
   Serial.println("");
 }
 
+// mostra su console seriale la distanza misurata dal sensore ultrasonico in centimetri
 void readDistance(){
-  // Clears the trigPin condition
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
-  // Displays the distance on the Serial Monitor
+  distance = duration * 0.034 / 2; 
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
-  //delay(100);
 }
 
+// legge la distanza misurata dal sensore ultrasonico in centimetri
 int readDistance_int(){
 
   digitalWrite(trigPin, LOW);
@@ -125,6 +111,7 @@ int readDistance_int(){
   return distance;
 }
 
+// scan deprecato
 void scan(){
   for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
@@ -139,6 +126,10 @@ void scan(){
   } 
 }
 
+// scannerizza di 180 gradi l'ambiente verso il quale è rivolto il rover
+// params:
+//  steps: il numero di misurazioni
+//  arr: l'array in cui verranno memorizzate le misurazioni (viene riallocato)
 void scanSteps(int steps, int *arr){
   arr = calloc(steps, sizeof(int));
   for (pos = 0; pos <= 180-1; pos += (180-1)/steps) { // goes from 0 degrees to 180 degrees
@@ -152,6 +143,7 @@ void scanSteps(int steps, int *arr){
   }
 }
 
+// ferma le ruote
 void stop(){
   digitalWrite(rf, LOW);
   digitalWrite(rb, LOW);
@@ -159,35 +151,43 @@ void stop(){
   digitalWrite(lb, LOW);
 }
 
+// muovla ruota destra avanti
 void right(){
   digitalWrite(rf, HIGH);
 }
 
+// muove la ruota destra indietro
 void bright(){
   digitalWrite(rb, HIGH);
 }
 
+// muove la ruota sinistra avanti
 void left(){
   digitalWrite(lf, HIGH);
 }
 
+// muove la ruota sinistra indietro
 void bleft(){
   digitalWrite(lb, HIGH);
 }
 
+// muove avanti
 void forw(){
   right();
   left();
 }
 
+// muove indietro
 void backw(){
   bright();
   bleft();
 }
 
+// test per le ruote
 void wheels_test(){
   right();
   delay(2000);
+
   stop();
   bright();
   delay(2000);
@@ -210,19 +210,166 @@ void wheels_test(){
   delay(1000);
 }
 
-void loop() {
-  /*
-  int *arr;
-  scanSteps(10, arr);
-  printa(10, arr);
-  */
-  /*
-  myservo.write(0);
-  delay(400); 
-  myservo.write(90);
-  delay(400); 
-  myservo.write(170);
-
-  */
+//test per le ruote che stampa anche i valori dei sensori di hall
+void wheels_test_hall(){
+  right();
+  read_hall(2000);
   
+
+  stop();
+  bright();
+  read_hall(2000);
+  stop();
+
+  left();
+  read_hall(2000);
+  stop();
+  bleft();
+  read_hall(2000);
+  stop();
+
+  forw();
+  read_hall(2000);
+  stop();
+  backw();
+  read_hall(2000);
+  stop();
+
+  delay(1000);
+}
+
+void read_hall(int ms){
+  for(int i=0; i<ms; i++){
+    if(i%50==0){
+      int hall_r = digitalRead(hall_pin_r);
+      int hall_l = digitalRead(hall_pin_l);
+      Serial.print(hall_l);
+      Serial.print(", ");
+      Serial.println(hall_r);
+    }
+    delay(1);
+  }
+}
+
+// legge il valore del sensore di hall destro
+// returns:
+//   0 se non viene rilevato un campo magnetico
+//   1 se viene rilevato
+int read_hall_r(){
+  return digitalRead(hall_pin_r);
+}
+
+// legge il valore del sensore di hall sinistro
+// returns:
+//   0 se non viene rilevato un campo magnetico
+//   1 se viene rilevato
+int read_hall_l(){
+  return digitalRead(hall_pin_l);
+}
+
+// ruota la/e ruota/e clicks volte.
+// params:
+//   clicks: il numero di punti d'appoggio da contare
+//   fun_rot: la funzione di rotazione
+//   fun_hall: la funzione di lettura del valore del sensore di hall
+void rotate_times(int clicks, void (*fun_rot)(), int (*fun_hall)()){
+  int init_hall_state = fun_hall();
+  int hall_state = 0;
+  while(clicks--){
+    if(init_hall_state){
+      do{
+        fun_rot();
+        hall_state = fun_hall();
+      }while(hall_state);
+      do{
+        fun_rot();
+        hall_state = fun_hall();
+      }while(!hall_state);
+    }else{
+      do{
+        fun_rot();
+        hall_state = fun_hall();
+      }while(!hall_state);
+    }
+  }
+  stop();
+}
+
+
+// Riposiziona le ruote sul primo punto di appoggio disponibile
+
+void calibrate_hall(){
+  rotate_times(1, right, read_hall_r);
+  rotate_times(1, left, read_hall_l);
+}
+
+void setup() {
+  #ifdef ENABLE_RADAR
+    myservo.attach(SERVO_PIN);
+    pinMode(trigPin, OUTPUT); 
+    pinMode(echoPin, INPUT); 
+  #endif
+  #ifdef ENABLE_MOTORS
+    pinMode(rf, OUTPUT); 
+    pinMode(rb, OUTPUT);
+    pinMode(lf, OUTPUT);
+    pinMode(lb, OUTPUT);
+  #endif
+
+  pinMode(hall_pin_r, INPUT);
+  pinMode(hall_pin_l, INPUT);
+
+
+  Serial.begin(9600);
+
+  //inizializza e visualizza mappa 
+  init_loc();
+  p_map();
+    
+  Serial.println("---begin---");
+  myservo.write(pos); //riposiziona servo
+
+  calibrate_hall(); //riposizioniamo le ruote
+  delay(2000); //per riposizionare la macchinina
+}
+
+void loop() {
+  
+  // int *arr;
+  // scanSteps(10, arr);
+  // printa(10, arr);
+  
+  // wheels_test_hall();
+  // delay(1000);
+
+  // right();
+  // read_hall(2000);
+  // stop();
+  // delay(1000);
+
+
+  // rotate_times(10, right, read_hall_r);
+  // delay(2000);
+
+  rotate_times(1, forw, read_hall_l); //21 cm
+  delay(1000);
+  
+  
+  // myservo.write(0);
+  // delay(400); 
+  // myservo.write(90);
+  // delay(400); 
+  // myservo.write(170);
+  // delay(400); 
+  
+  //scan();
+  //delay(100);
+  //myservo.write(0);
+
+  // int hall_r = digitalRead(hall_pin_r);
+  // int hall_l = digitalRead(hall_pin_l);
+  // Serial.print(hall_l);
+  // Serial.print(", ");
+  // Serial.println(hall_r);
+  // delay(100);
 }
